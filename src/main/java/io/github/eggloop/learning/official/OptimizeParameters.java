4849e2304b72b19efc37731ba3cef1b272490f51
Simone <simone.silvetti@gmail.com>
2019-06-17 10:03:26 +0200

    added classes for optimization of parameters

---
+++ src/main/java/io/github/eggloop/learning/official/OptimizeParameters.java
@@ -0,0 +1,44 @@
+package io.github.eggloop.learning.official;
+
+import io.github.eggloop.expr.Context;
+import io.github.eggloop.learning.Learning;
+import io.github.eggloop.mitl.MiTL;
+import io.github.eggloop.model.Trajectory;
+import io.github.eggloop.parsers.MitlFactory;
+import io.github.eggloop.utils.files.Utils;
+import io.github.eggloop.utils.string.StringUtils;
+
+import java.util.Arrays;
+import java.util.function.UnaryOperator;
+
+public class OptimizeParameters {
+    private static final UnaryOperator<String> FILE_PATH = Utils.getFilePath(Learning.class);
+    private static double[] ds2Times = Utils.readVectorFromFile(FILE_PATH.apply("temporal/synthTime_12.txt"));
+    private static double[][][] ds2SpatialValues = Utils.readMatrixMultiFromFile(ds2Times.length, FILE_PATH.apply("temporal/synthData_12.txt"));
+
+    //::phi:: G[Tl_176, Tu_176] ((y >= Theta_3 | z <= Theta_6))  ::score:: 1.90375763520218 ::parameters:: [11.0, 11.0, 48.990792215517644, 37.53885223004586]
+    public static void main(String[] args) {
+
+        String[] variables = new String[]{"y", "z"};
+        String[] parameters = new String[]{"Tl_176", "Tu_176", "Theta_3", "Theta_6"};
+        double[] parametersValues = new double[]{11.0, 11.0, 48.990792215517644, 37.53885223004586};
+        String formula = "G[Tl_176, Tu_176] ((y >= Theta_3 | z <= Theta_6))";
+        int positiveClassified = check(ds2Times, ds2SpatialValues, variables, parameters, formula, parametersValues, 0);
+        System.out.println("TOTAL:" + ds2SpatialValues.length);
+        System.out.println("POSITIVE CLASSIFIED:" + positiveClassified);
+    }
+
+    private static int check(double[] times, double[][][] trajectories, String[] variables, String[] parameters, String formula, double[] parameterValues, double atTime) {
+        int[] b = new int[trajectories.length];
+        Context ns = new Context(variables);
+        MitlFactory factory = new MitlFactory(ns);
+        String text = StringUtils.replace(formula, parameters, parameterValues) + "\n";
+        MiTL prop = factory.constructProperty(text);
+
+        for (int i = 0; i < trajectories.length; i++) {
+            Trajectory x = new Trajectory(times, ns, trajectories[i]);
+            b[i] = prop.evaluate(x, atTime) ? 1 : 0;
+        }
+        return Arrays.stream(b).sum();
+    }
+}
---
+++ src/main/java/io/github/eggloop/learning/official/SingleFormulaOptimization2.java
@@ -0,0 +1,61 @@
+package io.github.eggloop.learning.official;
+
+import io.github.eggloop.learning.ComputeAverage;
+import io.github.eggloop.learning.Learning;
+import io.github.eggloop.simhya.simhya.matlab.genetic.Formula;
+import io.github.eggloop.simhya.simhya.matlab.genetic.FormulaPopulation;
+import io.github.eggloop.utils.data.TrajectoryMultiReconstruction;
+import io.github.eggloop.utils.files.Utils;
+
+import java.util.Arrays;
+import java.util.Random;
+import java.util.function.BiFunction;
+import java.util.function.UnaryOperator;
+
+public class SingleFormulaOptimization2 {
+    private static final UnaryOperator<String> FILE_PATH = Utils.getFilePath(Learning.class);
+    private static final BiFunction<double[], double[], Double> DISCRIMINATION_FUNCTION = (x, y) -> (x[0] - y[0]) / (Math.abs(x[1] + y[1]));
+
+    public static void main(String[] args) throws InterruptedException {
+        Random ran = new Random();
+        //import database
+        double[] ds2Labels = Utils.readVectorFromFile(FILE_PATH.apply("ST_LIB_DATA/ev_spike/label.txt"));
+        double[] ds2Times = Utils.readVectorFromFile(FILE_PATH.apply("ST_LIB_DATA/ev_spike/time.txt"));
+        double[][][] ds2SpatialValues = Utils.readMatrixMultiFromFile(ds2Times.length, FILE_PATH.apply("ST_LIB_DATA/ev_spike/traj.txt"));
+        TrajectoryMultiReconstruction data = new TrajectoryMultiReconstruction(ds2Times, ds2SpatialValues, ds2Labels, 0.8, ran);
+        data.split();
+        double[][][] normal_model = data.getPositiveTrainingSet();
+        double[][][] ineffective_model = data.getNegativeTrainingSet();
+        double[][][] normal_model_test = data.getPoistiveTestSet();
+        double[][][] ineffective_model_test = data.getNegativeTestSet();
+
+        FormulaPopulation pop = new FormulaPopulation(1);
+        String[] variables = new String[]{"x", "y"};
+        //addVariableInformation to population
+        double[] lower = new double[]{0, 0};
+        double[] upper = new double[]{80, 45};
+        for (int i = 0; i < variables.length; i++) {
+            pop.addVariable(variables[i], lower[i], upper[i]);
+        }
+
+        String[] pars = new String[]{"Tl_1", "Tu_1", "Theta_1", "Theta_2"};
+        double[] vv = new double[]{0, 200, 20, 30};
+        //String FF = "P=?[ ({flow1 <= Theta_1}) U[Tl_1,Tu_1] ({flow <= Theta_2}) ]";
+       //rise := x_[t+d]-x_[t] > a
+        //fall := x_[t]-x_[t+d] > a
+//ev_spike := ev_[t_start, t_end-2*d] (x_[t+d]-x_[t] > a and (ev_[0, 2*d]  x_[t]-x_[t+d] > a ))
+
+
+        //rise and (ev_[0, 2*d] fall)
+
+        String FF = "P=?[({y >= Theta_1}) U[Tl_1, Tu_1] ({x <= Theta_2})]";
+        Formula formula = pop.loadFormula(FF, pars, vv);
+        double[] bestFormulaParameters = ComputeAverage.averageMultiTrajectory(200, DISCRIMINATION_FUNCTION, variables, ds2Times, normal_model, ineffective_model, formula, pop, new double[]{0, 300}, 0);
+        System.out.println("__________________________________________________");
+        System.out.println("bestFormula:" + formula.toString());
+        System.out.println("bestParam:" + Arrays.toString(formula.getParameters()) + ":::::" + Arrays.toString(bestFormulaParameters));
+        System.out.println("ineffectiveB= " + Arrays.toString(ComputeAverage.smcMultiTrajectories(ds2Times, ineffective_model_test, variables, formula, bestFormulaParameters, 0)));
+        System.out.println("normalB= " + Arrays.toString(ComputeAverage.smcMultiTrajectories(ds2Times, normal_model_test, variables, formula, bestFormulaParameters, 0)));
+    }
+}
+
