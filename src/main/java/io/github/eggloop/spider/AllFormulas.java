cd74f5e31d3b2638a10953d02e78b8b7c2acfcdf
lauranenzi <laura.nenzi@gmail.com>
2019-07-14 22:57:04 +0200

    provaGlob

--- src/main/java/io/github/eggloop/spider/AllFormulas.java
+++ src/main/java/io/github/eggloop/spider/AllFormulas.java
@@ -3,10 +3,7 @@ package io.github.eggloop.spider;
 import io.github.eggloop.expression.arithmetic.Assignment;
 import io.github.eggloop.expression.arithmetic.Variable;
 import io.github.eggloop.expression.relational.GreaterEqualTo;
-import io.github.eggloop.stl.syntax.Atom;
-import io.github.eggloop.stl.syntax.Finally;
-import io.github.eggloop.stl.syntax.Formula;
-import io.github.eggloop.stl.syntax.Interval;
+import io.github.eggloop.stl.syntax.*;
 import io.github.eggloop.stl.visitor.BooleanTemporalMonitoring;
 import io.github.eggloop.stl.visitor.FormulaPrinter;
 import io.github.eggloop.stl.visitor.LogicOperatorToken;
@@ -24,11 +21,11 @@ import java.util.stream.IntStream;
 public class AllFormulas {
 
     private void getTrajectory() throws IOException, ParseException {
-        String location = "/home/ssilvetti/git/eggloop/scripts/traccia.json";
+        String location = "/Users/lauretta/git/eggloop/scripts/traccia.json";
         String jsonTrajectory = FileUtils.readFileToString(location);
         Trajectory trajectory = TrajectoryFactory.fromJSON(jsonTrajectory);
         double[] times = trajectory.getTimes();
-        double[] space = IntStream.range(0, 20).mapToDouble(s -> 0.5 / 20. * s).toArray();
+        double[] space = IntStream.range(-1, 3).mapToDouble(s -> 0.1 * s).toArray();
         List<double[]> intervals = new ArrayList<>();
         for (int i = 0; i < times.length; i++) {
             double initTime = times[i];
@@ -40,7 +37,8 @@ public class AllFormulas {
         BooleanTemporalMonitoring booleanTemporalMonitoring = new BooleanTemporalMonitoring(trajectory);
         FormulaPrinter formulaPrinter = new FormulaPrinter(new LogicOperatorToken());
 
-        Formula prova = new Finally(new Interval(new Variable("a"), new Variable("b")), new Atom(new GreaterEqualTo(new Variable("X"), new Variable("k"))));
+        Formula provaf = new Finally(new Interval(new Variable("a"), new Variable("b")), new Atom(new GreaterEqualTo(new Variable("X"), new Variable("k"))));
+        Formula provag = new Globally(new Interval(new Variable("a"), new Variable("b")), new Atom(new GreaterEqualTo(new Variable("X"), new Variable("k"))));
 
         Function<double[], Assignment> ass = value -> {
             Assignment assignment = new Assignment();
@@ -51,8 +49,8 @@ public class AllFormulas {
         };
 
 
-        Function<double[], Boolean> eval = value -> prova.accept(booleanTemporalMonitoring).evaluate(ass.apply(value));
-        Function<double[], String> printer = value -> prova.accept(formulaPrinter).evaluate(ass.apply(value));
+        Function<double[], Boolean> eval = value -> provag.accept(booleanTemporalMonitoring).evaluate(ass.apply(value));
+        Function<double[], String> printer = value -> provag.accept(formulaPrinter).evaluate(ass.apply(value));
         for (double[] interval : intervals) {
             for (double spaceValue : space) {
                 double[] params = {spaceValue, interval[0], interval[1]};
