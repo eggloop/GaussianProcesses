c840360d30f55ac82262e68bd8945d6733422277
lauranenzi <laura.nenzi@gmail.com>
2019-07-15 11:19:01 +0200

    conj

--- src/main/java/io/github/eggloop/spider/AllFormulas.java
+++ src/main/java/io/github/eggloop/spider/AllFormulas.java
@@ -3,6 +3,7 @@ package io.github.eggloop.spider;
 import io.github.eggloop.expression.arithmetic.Assignment;
 import io.github.eggloop.expression.arithmetic.Variable;
 import io.github.eggloop.expression.relational.GreaterEqualTo;
+import io.github.eggloop.expression.relational.LowerEqualTo;
 import io.github.eggloop.stl.syntax.*;
 import io.github.eggloop.stl.visitor.BooleanTemporalMonitoring;
 import io.github.eggloop.stl.visitor.FormulaPrinter;
@@ -30,43 +31,59 @@ public class AllFormulas {
         String jsonTrajectory = FileUtils.readFileToString(inputLocation);
         Trajectory trajectory = TrajectoryFactory.fromJSON(jsonTrajectory);
         double[] times = trajectory.getTimes();
-        double[] space = IntStream.range(-1, 3).mapToDouble(s -> 0.1 * s).toArray();
+        double[] space = IntStream.range(-1, 20).mapToDouble(s -> 0.1 * s).toArray();
+
+        List<double[]> spaceIntervals = new ArrayList<>();
+        for (int i = 0; i < space.length; i++) {
+            double initSpace = space[i];
+            for (int j = i; j < times.length; j++) {
+                spaceIntervals.add(new double[]{initSpace, space[j]});
+            }
+        }
+
+
+
         List<double[]> intervals = new ArrayList<>();
         for (int i = 0; i < times.length; i++) {
             double initTime = times[i];
-            for (int j = i; j < times.length; j++) {
+            for (int j = i+2; j < times.length; j++) {
                 intervals.add(new double[]{initTime, times[j]});
             }
         }
         BooleanTemporalMonitoring booleanTemporalMonitoring = new BooleanTemporalMonitoring(trajectory);
         FormulaPrinter formulaPrinter = new FormulaPrinter(new LogicOperatorToken());
 
-        Formula provaf = new Finally(new Interval(new Variable("a"), new Variable("b")), new Atom(new GreaterEqualTo(new Variable("X"), new Variable("k"))));
-        Formula provag = new Globally(new Interval(new Variable("a"), new Variable("b")), new Atom(new GreaterEqualTo(new Variable("X"), new Variable("k"))));
+
+        Formula atoml = new Atom(new GreaterEqualTo(new Variable("X"), new Variable("h")));
+        Formula atomh = new Atom(new LowerEqualTo(new Variable("X"), new Variable("l")));
+        Formula prova = new Finally(new Interval(new Variable("a"), new Variable("b")), new Conjunction( atoml, atomh));
+        Formula provaf = new Finally(new Interval(new Variable("a"), new Variable("b")), atoml);
+        Formula provag = new Globally(new Interval(new Variable("a"), new Variable("b")),atoml );
 
         Function<double[], Assignment> ass = value -> {
             Assignment assignment = new Assignment();
-            assignment.put("k", value[0]);
-            assignment.put("a", value[1]);
-            assignment.put("b", value[2]);
+            assignment.put("h", value[0]);
+            assignment.put("l", value[1]);
+            assignment.put("a", value[2]);
+            assignment.put("b", value[4]);
             return assignment;
         };
 
 
-        Predicate<double[]> eval = value -> provag.accept(booleanTemporalMonitoring).evaluate(ass.apply(value));
-        getString(stringBuilder, space, intervals, formulaPrinter, provag, ass, eval);
-        getString(stringBuilder, space, intervals, formulaPrinter, provaf, ass, eval);
+        Predicate<double[]> eval = value -> prova.accept(booleanTemporalMonitoring).evaluate(ass.apply(value));
+        getString(stringBuilder, spaceIntervals, intervals, formulaPrinter, prova, ass, eval);
+        //getString(stringBuilder, spaceIntervals, intervals, formulaPrinter, provaf, ass, eval);
         Path path = Paths.get(outputLocation);
         try (BufferedWriter writer = Files.newBufferedWriter(path)) {
             writer.write(stringBuilder.toString());
         }
     }
 
-    private static void getString(StringBuilder stringBuilder, double[] space, List<double[]> intervals, FormulaPrinter formulaPrinter, Formula provag, Function<double[], Assignment> ass, Predicate<double[]> eval) {
-        Function<double[], String> printer = value -> provag.accept(formulaPrinter).evaluate(ass.apply(value));
+    private static void getString(StringBuilder stringBuilder, List<double[]> spaceIntervals, List<double[]> intervals, FormulaPrinter formulaPrinter, Formula prova, Function<double[], Assignment> ass, Predicate<double[]> eval) {
+        Function<double[], String> printer = value -> prova.accept(formulaPrinter).evaluate(ass.apply(value));
         for (double[] interval : intervals) {
-            for (double spaceValue : space) {
-                double[] params = {spaceValue, interval[0], interval[1]};
+            for (double [] spaceInterval : spaceIntervals) {
+                double[] params = {spaceInterval[0], spaceInterval[1], interval[0], interval[1]};
                 boolean test = eval.test(params);
                 if(test) {
                     stringBuilder.append(printer.apply(params)).append("\n");
